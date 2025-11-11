247eb547b8cc7dbc9a1071ffde62bc7d086f8ebd
Matt Benton <mattbenton@hey.com>
2025-11-11 09:16:06 -0600

    Add triforce package

--- lua/user/plugins/coding.lua
+++ lua/user/plugins/coding.lua
@@ -34,5 +34,18 @@ return {
 		      vim.keymap.set('n', 'gm', '<plug>(VesselSetGlobalMark)')
 		    })
 		  end,
-		},
+	{
+  "gisketch/triforce.nvim",
+  dependencies = {
+    "nvzone/volt",
+  },
+  config = function()
+    require("triforce").setup({
+      -- Optional: Add your configuration here
+      keymap = {
+        show_profile = "<leader>tp", -- Open profile with <leader>tp
+      },
+    })
+  end,
+}	},
 }
