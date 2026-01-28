0409431c39bc8747110a237bcf9f96e61212dcf7
Matt Benton <mattbenton@hey.com>
2026-01-28 15:26:26 -0600

    Disable formatting for vue files

--- lua/user/plugins/formatting.lua
+++ lua/user/plugins/formatting.lua
@@ -8,11 +8,21 @@ return {
                     lua = { "stylua" },
                     php = { "pint" },
                 },
-                format_on_save = {
-                    -- These options will be passed to conform.format()
-                    timeout_ms = 500,
-                    lsp_format = "fallback",
-                },
+                format_on_save = function(bufnr)
+                    -- Disable formatting on save for Vue files.
+                    if vim.bo[bufnr].filetype == "vue" then
+                        return {
+                            timeout_ms = 500,
+                            lsp_format = "never",
+                        }
+                    end
+
+                    -- Otherwise, use LSP formatting only when no other formatter is configured.
+                    return {
+                        timeout_ms = 500,
+                        lsp_format = "fallback",
+                    }
+                end,
             }
             return opts
         end,
