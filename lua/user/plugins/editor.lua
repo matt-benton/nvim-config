7d5b2dab6c24abe66eb86b9431d6641b3b7134dc
Matt Benton <mattbenton@hey.com>
2026-09-11 14:55:26 -0500

    Change theme to everforest

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -52,19 +52,12 @@ return {
         end,
     },
     {
-        "rebelot/kanagawa.nvim",
+        "sainnhe/everforest",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            require("kanagawa").setup({
-                theme = "dragon",
-                background = {
-                    dark = "dragon",
-                    light = "lotus",
-                },
-            })
-
-            vim.cmd("colorscheme kanagawa-dragon")
+            vim.g.everforest_background = "medium"
+            vim.cmd("colorscheme everforest")
         end,
     },
     {
